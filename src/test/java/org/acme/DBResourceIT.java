e1a28343dc0bf44fe5142743c42abf219197175c
Maximilian Hartmann <97613318+maximilian-hartmann@users.noreply.github.com>
2022-06-01 07:07:43 +0000

    remove db resources test

--- src/test/java/org/acme/DBResourceIT.java
+++
@@ -1,9 +0,0 @@
-package org.acme;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class DBResourceIT extends DBResourceTest {
-
-    // Execute the same tests but in native mode.
-}
